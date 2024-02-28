d24ffdef46ac2f9b83d5884ab1608e57a561f4aa
ebhimisetty <ebhimisetty@adobe.com>
2024-02-28 21:47:17 +0530

    first commit

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,10 @@
+const DashboardLayout = ({ children }) => {
+  return (
+    <div>
+      <h1>Dashboard Layout v2</h1>
+      <div>{children}</div>
+    </div>
+  )
+}
+
+export default DashboardLayout;
---
+++ app/docs/[[...id]]/page.tsx
@@ -0,0 +1,12 @@
+const DocsPage = ({
+  params
+}) => {
+  console.log('EAEA params', params);
+  return (
+    <div>
+      <h1>Docs Page</h1>
+    </div>
+  );
+}
+
+export default DocsPage;
